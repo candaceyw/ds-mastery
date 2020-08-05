a1c4ef6cc5425b0b9702fdd32289799b996a5097
candaceyw <candaceyw@gmail.com>
2020-08-05 10:43:07 -0400

    hashtable exercise

--- hashtables.js
+++ hashtables.js
@@ -12,3 +12,7 @@ user.spell = 'abra kadabra'; // O(1)
 user.scream(); //O(1)
 
 // https://www.cs.usfca.edu/~galles/visualization/OpenHash.html Open hashing visualization tool
+
+// Hashtable examples
+const a = new Map();
+const b = new Sets();
